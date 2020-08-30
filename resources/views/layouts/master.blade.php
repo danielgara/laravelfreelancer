b456d8b993f93c342f7d0b50a62a82b696b3629d
DanielGara <yo@danielgara.com>
2020-08-30 13:36:10 -0500

    Fix: assets path

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,14 +7,14 @@
         <meta name="author" content="" />
         <title>@yield('title','Home Page')</title>
         <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.ico') }}" />
+        <link rel="icon" type="image/x-icon" href="{{ asset('/img/favicon.ico') }}" />
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+        <link href="{{ asset('/css/styles.css') }}" rel="stylesheet" />
     </head>
     <body id="page-top">
         <!-- Navigation-->
